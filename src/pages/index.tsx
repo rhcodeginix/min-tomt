52c1086774a5608dc857415cb728b61b6facc40b
Drashti Savani <drashtisavani22@gmail.com>
2025-08-29 15:15:43 +0530

    add some change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -79,6 +79,8 @@ const index = () => {
               const userData = existingUserDoc.data();
               const userDocRef = existingUserDoc.ref;
 
+              console.log(userData);
+
               if (
                 userData.loginType === "form" ||
                 userData.loginType === "google"
@@ -92,8 +94,6 @@ const index = () => {
                 );
                 return;
               }
-              await createUserWithEmailAndPassword(auth, userEmail, userUid);
-              console.log("------111");
 
               await signInWithEmailAndPassword(auth, userEmail, userUid);
               console.log("------");
