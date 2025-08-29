d15080d0b7d0a69bc667026aa36cc980aa7e1de1
Drashti Savani <drashtisavani22@gmail.com>
2025-08-29 16:56:10 +0530

    add some change

--- src/components/Ui/navbar/index.tsx
+++ src/components/Ui/navbar/index.tsx
@@ -105,7 +105,7 @@ const Header = () => {
     });
 
     return () => unsubscribe();
-  }, []);
+  }, [router.query]);
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       if (
