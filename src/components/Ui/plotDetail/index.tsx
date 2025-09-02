30dad192d36da86e5e9a5d7d3d587e11f1a395f2
Drashti Savani <drashtisavani22@gmail.com>
2025-09-02 17:27:40 +0530

    add some change

--- src/components/Ui/plotDetail/index.tsx
+++ src/components/Ui/plotDetail/index.tsx
@@ -2016,7 +2016,7 @@ const PlotDetailPage: React.FC<{
                 </>
               ) : (
                 <>
-                  {PlanDocuments ? (
+                  {PlanDocuments && PlanDocuments?.length > 0 ? (
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                       {PlanDocuments.map((doc: any, index: number) => (
                         <DocumentCard
@@ -2050,7 +2050,7 @@ const PlotDetailPage: React.FC<{
                 </>
               ) : (
                 <>
-                  {exemptions ? (
+                  {exemptions && exemptions?.length > 0 ? (
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                       {exemptions.map((doc: any, index: number) => (
                         <DocumentCard
