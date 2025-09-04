f933fc81b2587130f8719878e3cfe2c530ea4c1e
Drashti Savani <drashtisavani22@gmail.com>
2025-09-04 11:31:55 +0530

    add new api

--- src/components/Ui/plotDetail/index.tsx
+++ src/components/Ui/plotDetail/index.tsx
@@ -2094,10 +2094,12 @@ const PlotDetailPage: React.FC<{
               ) : (
                 <>
                   {(() => {
+                    const allDocs = [
+                      KommunePlan?.rule_book,
+                      ...(KommunePlan?.planning_documents || []),
+                    ];
                     const filteredDocs =
-                      KommunePlan?.planning_documents?.filter(
-                        (doc: any) => doc && doc.link
-                      ) || [];
+                      allDocs?.filter((doc: any) => doc && doc.link) || [];
 
                     return filteredDocs.length > 0 ? (
                       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
